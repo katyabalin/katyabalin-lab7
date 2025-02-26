740349337ed94c1cc0d495c6622c98ab860f46ef
katyabalin <katya.balin@gmail.com>
2025-02-26 11:28:28 -0500

    Create TypeB.java

---
+++ TypeB.java
@@ -0,0 +1,19 @@
+class TypeB extends Thing {
+    private int stepsSinceLast;
+    
+    public TypeB(int x, int y) {
+        super(x, y, 'b');
+        this.stepsSinceLast = 0;
+    }
+    
+    @Override
+    public void decideTurn() {
+        stepsSinceLast++;
+        if (stepsSinceLast == 10) {
+            stepsSinceLast = 0;
+            int choice = randomGen.nextInt(3);
+            if (choice == 1) turnRight();
+            if (choice == 2) turnLeft();
+        }
+    }
+}
