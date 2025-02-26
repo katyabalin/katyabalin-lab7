869a1936a87ca5888b53a23792c66c2e7f6957af
katyabalin <katya.balin@gmail.com>
2025-02-26 11:30:22 -0500

    Create ThingList.java

---
+++ ThingList.java
@@ -0,0 +1,38 @@
+class ThingList {
+    private class Node {
+        Thing data;
+        Node next;
+        
+        Node(Thing data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    
+    private Node head;
+    
+    public ThingList() {
+        this.head = null;
+    }
+    
+    public void addThing(Thing thing) {
+        Node newNode = new Node(thing);
+        newNode.next = head;
+        head = newNode;
+    }
+    
+    public void moveAll() {
+        for (Node current = head; current != null; current = current.next) {
+            current.data.decideTurn();
+            current.data.move();
+        }
+    }
+    
+    public void displayAll() {
+        for (Node current = head; current != null; current = current.next) {
+            System.out.println(current.data);
+        }
+        System.out.println("done");
+        System.out.flush();
+    }
+}
