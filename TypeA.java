0d6d6080d0dc6994f11ede7cb88413c2fd5e56f8
katyabalin <katya.balin@gmail.com>
2025-02-26 11:53:57 -0500

    fixed rightTurn leftTurn

--- TypeA.java
+++ TypeA.java
@@ -6,7 +6,7 @@ class TypeA extends Thing {
     @Override
     public void decideTurn() {
         int choice = randomGen.nextInt(3);
-        if (choice == 1) turnRight();
-        if (choice == 2) turnLeft();
+        if (choice == 1) rightTurn();
+        if (choice == 2) leftTurn();
     }
 }
