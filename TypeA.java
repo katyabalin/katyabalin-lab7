c2a1a7876f4a4a69744028f530265d4925805a96
katyabalin <katya.balin@gmail.com>
2025-02-26 12:08:04 -0500

    fixed format

--- TypeA.java
+++ TypeA.java
@@ -1,4 +1,4 @@
-class TypeA extends Thing {
+public class TypeA extends Thing {
     public TypeA(int x, int y) {
         super(x, y, 'r');
     }
@@ -6,7 +6,10 @@ class TypeA extends Thing {
     @Override
     public void decideTurn() {
         int choice = randomGen.nextInt(3);
-        if (choice == 1) rightTurn();
-        if (choice == 2) leftTurn();
+        if (choice == 1) {
+            rightTurn();
+        } else if (choice == 2) {
+            leftTurn();
+        }
     }
 }
