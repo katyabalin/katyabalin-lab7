88d94bc5a4ed583170bdd570c9fc2d81c1b05528
katyabalin <katya.balin@gmail.com>
2025-02-28 18:58:51 -0500

    updated to mark Node as private

--- ThingList.java
+++ ThingList.java
@@ -2,35 +2,39 @@ public class ThingList {
     private class Node {
         Thing data;
         Node next;
-        
+
         Node(Thing data) {
             this.data = data;
             this.next = null;
         }
     }
-    
+
     private Node head;
-    
+
     public ThingList() {
         this.head = null;
     }
-    
+
     public void addThing(Thing thing) {
         Node newNode = new Node(thing);
         newNode.next = head;
         head = newNode;
     }
-    
+
     public void moveAll() {
-        for (Node current = head; current != null; current = current.next) {
+        Node current = head;
+        while (current != null) {
             current.data.decideTurn();
             current.data.step();
+            current = current.next;
         }
     }
-    
+
     public void displayAll() {
-        for (Node current = head; current != null; current = current.next) {
+        Node current = head;
+        while (current != null) {
             System.out.println(current.data);
+            current = current.next;
         }
         System.out.println("done");
         System.out.flush();
