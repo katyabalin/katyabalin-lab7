97298ddfa6d79fc96d161fa3cfd6db925d95e775
katyabalin <katya.balin@gmail.com>
2025-02-28 18:58:26 -0500

    updated to only have movement methods here

--- Thing.java
+++ Thing.java
@@ -4,32 +4,32 @@ public abstract class Thing {
     protected int x, y, direction;
     protected char label;
     protected Random randomGen;
-    
+
     public Thing(int x, int y, char label) {
         this.x = x;
         this.y = y;
         this.label = label;
-        this.direction = 0; 
+        this.direction = 0; // the default direction is North
         this.randomGen = new Random();
     }
-    
+
     public void rightTurn() {
         this.direction = (this.direction + 1) % 4;
     }
-    
+
     public void leftTurn() {
         this.direction = (this.direction + 3) % 4;
     }
-    
+
     public void step() {
-        final int[] deltaX = {0, 1, 0, -1};  
-        final int[] deltaY = {-1, 0, 1, 0};  
+        final int[] deltaX = {0, 1, 0, -1}; // north, east, south, west
+        final int[] deltaY = {-1, 0, 1, 0}; // this is coordinate-based movement
         this.x += deltaX[this.direction];
         this.y += deltaY[this.direction];
     }
-    
-    public abstract void decideTurn();
-    
+
+    public abstract void decideTurn(); // makes sure there is  abstraction
+
     @Override
     public String toString() {
         return x + " " + y + " " + label;
