0b2d4e3141046e288d271fe300e10bd7f4bc4d46
katyabalin <katya.balin@gmail.com>
2025-03-02 15:24:43 -0500

    updated so that moveAll has random

--- DotChaser.java
+++ DotChaser.java
@@ -2,29 +2,22 @@ import java.util.Random;
 
 public class DotChaser {
     public static void main(String[] args) {
-        int N = 200; // Default value for adding new dots
+        int N = 200;
+        if (args.length != 0) N = Integer.parseInt(args[0]);
 
-        if (args.length != 0) {
-            N = Integer.parseInt(args[0]);
-        }
-
-        ThingList thingList = new ThingList(); // Using a linked list instead of a Node structure
+        ThingList thingList = new ThingList();
+        Random rand = new Random();  // ✅ Create Random instance
         int count = 0;
 
         while (true) {
-            // Every N rounds, add new Thing objects (TypeA, TypeB, TypeC)
             if (count % N == 0) {
-                thingList.addThing(new TypeA(45, 50)); // Red
-                thingList.addThing(new TypeB(55, 50)); // Blue
-                thingList.addThing(new TypeC(50, 50)); // Green (Custom movement)
+                thingList.addThing(new TypeA(45, 50));
+                thingList.addThing(new TypeB(55, 50));
+                thingList.addThing(new TypeC(50, 50)); // Custom TypeC
             }
 
-            // Print the list of Things (each round)
             thingList.printAll();
-
-            // Move all Things
-            thingList.moveAll();
-
+            thingList.moveAll(rand);  // ✅ Pass Random object to moveAll
             count++;
         }
     }
