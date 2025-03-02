3d93bcea4f3c56bac618878f3f29494368559e18
katyabalin <katya.balin@gmail.com>
2025-03-02 15:24:19 -0500

    added Random rand to maybeTurn

--- TypeC.java
+++ TypeC.java
@@ -6,7 +6,7 @@ public class TypeC extends Thing {
     }
 
     @Override
-    public void maybeTurn() {
+    public void maybeTurn(Random rand) {
         stepCount++;
         if (stepCount % 2 == 0) rightTurn();
         else leftTurn();
