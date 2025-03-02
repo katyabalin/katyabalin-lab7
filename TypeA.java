b696a2145dd7119579b424d78b5b12188943be3b
katyabalin <katya.balin@gmail.com>
2025-03-02 15:23:30 -0500

    added Random rand to maybeTurn(Random rand)

--- TypeA.java
+++ TypeA.java
@@ -4,7 +4,7 @@ public class TypeA extends Thing {
     }
 
     @Override
-    public void maybeTurn() {
+    public void maybeTurn(Random rand) {
         int i = rand.nextInt(3);
         if (i == 1) rightTurn();
         if (i == 2) leftTurn();
