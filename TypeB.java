86198c6b790098e88bc9fc0f4a8a1e4aeaa0e56a
katyabalin <katya.balin@gmail.com>
2025-03-02 17:30:23 -0500

    added comments

--- TypeB.java
+++ TypeB.java
@@ -1,21 +1,24 @@
 import java.util.Random;
 
+// represents TypeB which is a subclass of thing
 public class TypeB extends Thing {
-    private int timeSinceLast;
+    private int timeSinceLast; // keeps track of steps taken since last turn
 
+    // constructor initializes TypeB with a starting position and assigns 'b' as its label
     public TypeB(int row, int col) {
-        super(row, col, 'b');
-        this.timeSinceLast = 0;
+        super(row, col, 'b'); // calls the parent constructor with 'b' as the identifier
+        this.timeSinceLast = 0; // sets the step counter to zero
     }
 
     @Override
+    // determines if the object should turn based on step count and randomness
     public void maybeTurn(Random rand) {
-        timeSinceLast++;
-        if (timeSinceLast == 10) {
-            timeSinceLast = 0;
-            int i = rand.nextInt(3);
-            if (i == 1) rightTurn();
-            if (i == 2) leftTurn();
+        timeSinceLast++; // increases the step counter
+        if (timeSinceLast == 10) { // checks if 10 steps have been taken
+            timeSinceLast = 0; // resets the counter
+            int i = rand.nextInt(3); // generates a random number between 0 and 2
+            if (i == 1) rightTurn(); // turns right if the number is 1
+            if (i == 2) leftTurn();  // turns left if the number is 2
         }
     }
 }
